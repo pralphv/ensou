18ff3f143c9d6f74674a661b421b343b84a98c68
pralphv <chowzhiho321@gmail.com>
2021-05-02 00:49:56 +0800

    implement key binding text

--- src/canvas/background/bottomTiles.ts
+++ src/canvas/background/bottomTiles.ts
@@ -1,6 +1,7 @@
 import * as PIXI from "pixi.js";
-import { PIANO_TUNING } from "audio/constants";
-import { BOTTOM_TILE_HEIGHT } from "../constants";
+import { PIANO_TUNING, NOTE_KEYBOARD_LABEL } from "audio/constants";
+
+import * as types from "../types";
 
 export default class BottomTiles {
   _app: PIXI.Application;
@@ -8,10 +9,13 @@ export default class BottomTiles {
   leftPadding: number;
   whiteKeyWidth: number;
   blackKeyWidth: number;
+  _config: types.IMyCanvasConfig;
+  textContainer: PIXI.Container;
 
-  constructor(app: PIXI.Application) {
+  constructor(app: PIXI.Application, config: types.IMyCanvasConfig) {
     this._app = app;
     this._container = new PIXI.Container();
+    this._config = config;
 
     this._app.stage.addChild(this._container);
     this._app.stage.setChildIndex(
@@ -23,15 +27,18 @@ export default class BottomTiles {
     const blackKeyWidth = Math.floor(whiteKeyWidth * 0.55);
 
     const leftPadding = (this._app.screen.width - whiteKeyWidth * 52) * 0.75;
-    console.log({ whiteKeyWidth, width: this._app.screen.width, leftPadding });
     this.leftPadding = leftPadding;
     this.whiteKeyWidth = whiteKeyWidth;
     this.blackKeyWidth = blackKeyWidth;
 
-    const whiteKey = initRectangle(whiteKeyWidth, BOTTOM_TILE_HEIGHT, true);
+    const whiteKey = initRectangle(
+      whiteKeyWidth,
+      this._config.bottomTileHeight,
+      true
+    );
     const blackKey = initRectangle(
       blackKeyWidth,
-      BOTTOM_TILE_HEIGHT * 0.66,
+      this._config.bottomTileHeight * 0.66,
       true
     );
 
@@ -44,27 +51,42 @@ export default class BottomTiles {
     let lastI: number; // to prevent duplicate notes from b and #
     const whiteKeyContainer = new PIXI.Container();
     const blackKeyContainer = new PIXI.Container();
+    this.textContainer = new PIXI.Container();
 
     Object.entries(PIANO_TUNING).forEach(([key, i]: [string, number]) => {
       if (i !== lastI) {
         lastI = i;
         const isBlackKey = key.includes("#") || key.includes("b");
         let sprite: PIXI.Sprite;
+        let text = new PIXI.Text(NOTE_KEYBOARD_LABEL[key], {
+          fontFamily: "Arial",
+          fontSize: 12,
+          fill: 0x7fdded,
+          align: "center",
+        });
+        this.textContainer.addChild(text);
         if (isBlackKey) {
           sprite = new PIXI.Sprite(blackKeyTexture);
           blackKeyContainer.addChild(sprite);
           sprite.position.x = x - blackKeyWidth / 2;
+          text.position.x = x - blackKeyWidth / 4;
+          text.position.y =
+            app.screen.height - this._config.bottomTileHeight * 0.66;
         } else {
           sprite = new PIXI.Sprite(whiteKeyTexture);
           whiteKeyContainer.addChild(sprite);
           sprite.position.x = x;
+          text.position.x = x + whiteKeyWidth / 3;
+          text.position.y = app.screen.height - 15;
           x += whiteKeyWidth;
         }
-        sprite.position.y = app.screen.height - 51;
+        sprite.position.y =
+          app.screen.height - this._config.bottomTileHeight - 1;
       }
     });
     this._container.addChild(whiteKeyContainer);
     this._container.addChild(blackKeyContainer);
+    this._container.addChild(this.textContainer);
 
     // black top overlay. should not be here
     const blackRect = initBlackColorOverlay(
@@ -77,9 +99,17 @@ export default class BottomTiles {
     sprite.position.x = 0;
     sprite.position.y = 0;
     this._container.addChild(sprite);
-
     whiteKey.destroy({ children: true, baseTexture: true, texture: true });
     blackKey.destroy({ children: true, baseTexture: true, texture: true });
+    this.hideText();
+  }
+
+  showText() {
+    this.textContainer.visible = true;
+  }
+
+  hideText() {
+    this.textContainer.visible = false;
   }
 
   destroy() {
