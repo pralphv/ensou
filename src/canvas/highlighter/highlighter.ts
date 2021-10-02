cfbfb07888517c31578035e7322bc2903535eb5e
Vincent <chowzhiho321@gmail.com>
2021-10-02 16:52:31 -0400

    make highlighter resize instead of rebuild

--- src/canvas/highlighter/highlighter.ts
+++ src/canvas/highlighter/highlighter.ts
@@ -12,27 +12,27 @@ export default class Highlighter {
     console.log("Constructing Highlighter");
     this._app = app;
     this._config = config;
+    const rect = initRectangle(this._app.screen.width, this._app.screen.height);
+    // @ts-ignore
+    this._texture = this._app.renderer.generateTexture(rect);
+    this._sprite = new PIXI.Sprite(this._texture);
+    this._app.stage.addChild(this._sprite);
+    this._sprite.visible = false;
+    rect.destroy({ children: true, texture: true, baseTexture: true });
   }
 
   draw() {
-    this.destroy();
+    this._sprite.visible = true;
+
+    // this.destroy();
     const startY = convertMidiTickToCanvasHeight(
       myMidiPlayer.playRange.startTick || 0
     );
     const endY = convertMidiTickToCanvasHeight(
       myMidiPlayer.playRange.endTick || 0
     );
-
-    const rect = initRectangle(this._app.screen.width, Math.abs(endY - startY));
-    // @ts-ignore
-    const texture = this._app.renderer.generateTexture(rect);
-    const sprite = new PIXI.Sprite(texture);
-    sprite.position.y = endY > startY ? startY : endY;
-    this._app.stage.addChild(sprite);
-    this._sprite = sprite;
-    this._texture = texture;
-    // canBeDestroyed = true;
-    rect.destroy({ children: true, texture: true, baseTexture: true });
+    this._sprite.position.y = endY > startY ? startY : endY;
+    this._sprite.height = Math.max(Math.abs(endY - startY), 2);
   }
 
   destroy() {
@@ -47,7 +47,6 @@ function initRectangle(width: number, height: number): PIXI.Graphics {
   const rect = new PIXI.Graphics();
   rect.beginFill(0x008cd9);
   rect.alpha = 0.3;
-  rect.lineStyle(3, 0x4dafff, 0.6);
   rect.drawRect(0, 0, width * 0.999, height);
   rect.endFill();
   return rect;
